40b3227f8d8c1b9efebcbf0e4c59bff89215506f
KoukiYonetani <39yonegla@gmail.com>
2022-09-27 20:17:39 +0900

    ver2.00.02 更新 ・各種メニューのフラグメント化 ・コンパイル時のパラメータ変更：非推奨クラス・apiのコンパイルエラー化 ・JobIntentService非推奨化対応 ・Javadoc追加対応

--- app/src/main/java/com/example/androidsample/customgui/CustomImageView.java
+++ app/src/main/java/com/example/androidsample/customgui/CustomImageView.java
@@ -7,6 +7,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageView;
 
+/**
+ * ImageViewSampe0302などで使用するカスタムイメージViewです。
+ *
+ **************************************
+ * 変更履歴:
+ * ver1.00 新規作成
+ * ver2.00 Javadoc追加対応
+ *
+ */
 public class CustomImageView extends AppCompatImageView {
     public CustomImageView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);

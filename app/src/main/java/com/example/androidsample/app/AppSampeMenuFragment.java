939d7092b18efc30fc6b4e1ec6af48bd9dfdbd71
KoukiYonetani <39yonegla@gmail.com>
2022-10-03 14:56:03 +0900

    ver2.00.02 更新 ・各種メニューのフラグメント化 ・コンパイル時のパラメータ変更：非推奨クラス・apiのコンパイルエラー化 ・JobIntentService非推奨化対応 ・Javadoc追加対応

--- app/src/main/java/com/example/androidsample/app/AppSampeMenuFragment.java
+++ app/src/main/java/com/example/androidsample/app/AppSampeMenuFragment.java
@@ -1,4 +1,3 @@
-package com.example.androidsample.app;
 /*
  * WEBのアンドロイドプログラミングサンプルをまとめて一つのプロジェクトとしました。
  * 参考ページ:nyanのアプリ開発：Androidアプリ版
@@ -11,32 +10,53 @@ package com.example.androidsample.app;
  * 13.Service
  *
  */
+package com.example.androidsample.app;
+
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
+import com.example.androidsample.AbstractMenuListFragment;
 import com.example.androidsample.common.CallUnderConstructionActivity;
 import com.example.androidsample.R;
 
-public class AppSampeMenu extends AppCompatActivity implements AdapterView.OnItemClickListener {
+/**
+ * トップメニューにて以下選択時に対応するフラグメントです
+ * 「7. アプリ(Activity, Fragment, Service)」
+ *
+ * メニューリストの表示項目設定と項目選択時のリスナーを実装します。
+ *
+ **************************************
+ * 変更履歴:
+ * ver1.00 新規作成
+ * ver2.00 更新
+ * 1.トップメニュー、サブメニューをフラグメントに変更
+ *    AbstractMenuList継承にてAppSampeMenuをフラグメントに変更
+ * 2.javadoc追加対応
+ *
+ */
+public class AppSampeMenuFragment extends AbstractMenuListFragment {
+    private String title;
+    /**
+     * このフラグメントのインスタンスを生成して返します。
+     *
+     * @param title 表示するタイトル
+     * @return このフラグメントのインスタンス
+     */
+    public static AppSampeMenuFragment newInstance(String title) {
+        AppSampeMenuFragment fragment = new AppSampeMenuFragment();
+        // パラメータを設定
+        fragment.title = title;
+        return fragment;
+    }
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // リストを設定
-        ListView menuList = findViewById(R.id.list_view);
-        menuList.setAdapter(ArrayAdapter.createFromResource(
-                getApplicationContext(),
-                R.array.app_menu,
-                android.R.layout.simple_list_item_1
-        ));
-        menuList.setOnItemClickListener(this);
+    protected String getTitleMessage() {
+        return title;
+    }
+
+    @Override
+    protected int getTextArrayResId() {
+        return R.array.app_menu;
     }
 
     @Override
@@ -45,59 +65,59 @@ public class AppSampeMenu extends AppCompatActivity implements AdapterView.OnIte
         switch(position) {
             case 0:
                 // [Activity]Activityの画面遷移(main⇔sub)
-                intent = new Intent(getApplicationContext(), ActivitySampe0101.class);
+                intent = new Intent(getContext(), ActivitySampe0101.class);
                 break;
             case 1:
                 // [Activity]Activity(main⇔sub)間のデータ受け渡し
-                intent = new Intent(getApplicationContext(), ActivitySampe0201.class);
+                intent = new Intent(getContext(), ActivitySampe0201.class);
                 break;
             case 2:
                 // [Activity]グローバル変数(アプリケーション内の共有データ)を参照する＋mainとsubの
                 // ライフサイクル確認
-                intent = new Intent(getApplicationContext(), ActivitySampe0301.class);
+                intent = new Intent(getContext(), ActivitySampe0301.class);
                 break;
             case 3:
                 // [Activity]Activity Life Cycle と画面回転
-                intent = new Intent(getApplicationContext(), ActivitySampe0401.class);
+                intent = new Intent(getContext(), ActivitySampe0401.class);
                 break;
             case 4:
                 // [Fragment]Fragmentを「Hello World」から始める
-                intent = new Intent(getApplicationContext(), FragmentSampe0101.class);
+                intent = new Intent(getContext(), FragmentSampe0101.class);
                 break;
             case 5:
                 // [Fragment]Fragmentタグ部分をコードで動的に設定する
-                intent = new Intent(getApplicationContext(), FragmentSampe0201.class);
+                intent = new Intent(getContext(), FragmentSampe0201.class);
                 break;
             case 6:
                 // [Fragment]画面遷移(Activity⇔Fragment)
-                intent = new Intent(getApplicationContext(), FragmentSampe0301.class);
+                intent = new Intent(getContext(), FragmentSampe0301.class);
                 break;
             case 7:
                 // 画面遷移(Fragment⇔Fragment)
-                intent = new Intent(getApplicationContext(), FragmentSampe0401.class);
+                intent = new Intent(getContext(), FragmentSampe0401.class);
                 break;
             case 8:
                 // 画面遷移(Fragment⇔Fragment)貼り付け部分の通知版
-                intent = new Intent(getApplicationContext(), FragmentSampe0402.class);
+                intent = new Intent(getContext(), FragmentSampe0402.class);
                 break;
             case 9:
                 // [Service]Serviceの使い方
-                intent = new Intent(getApplicationContext(), ServiceSampe0101.class);
+                intent = new Intent(getContext(), ServiceSampe0101.class);
                 break;
             case 10:
                 // [Service]WindowManagerを使ってServiceから画像を表示させ続ける
-                intent = new Intent(getApplicationContext(), ServiceSampe0201.class);
+                intent = new Intent(getContext(), ServiceSampe0201.class);
                 break;
             case 11:
                 // [Service]JobIntentServiceを使った(簡単な)バックグラウンド処理
-                intent = new Intent(getApplicationContext(), ServiceSampe0301.class);
+                intent = new Intent(getContext(), ServiceSampe0301.class);
                 break;
             case 12:
                 // [Service]JobIntentServiceを使ってバックグラウンドでゲーム効果音を再生
-                intent = new Intent(getApplicationContext(), ServiceSampe0302.class);
+                intent = new Intent(getContext(), ServiceSampe0302.class);
                 break;
             default:
-                intent = new Intent(getApplicationContext(), CallUnderConstructionActivity.class);
+                intent = new Intent(getContext(), CallUnderConstructionActivity.class);
                 intent.putExtra("position", position);
                 intent.putExtra("id", id);
         }

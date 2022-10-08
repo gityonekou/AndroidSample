d414fc78dc66f6fd172a68d6e236d1c126ce3b1d
KoukiYonetani <39yonegla@gmail.com>
2022-10-09 00:45:09 +0900

    ver2.00.02 更新 ・各種メニューのフラグメント化 ・コンパイル時のパラメータ変更：非推奨クラス・apiのコンパイルエラー化 ・JobIntentService非推奨化対応 ・Javadoc追加対応

--- app/src/main/java/com/example/androidsample/app/ServiceSampe0301.java
+++ app/src/main/java/com/example/androidsample/app/ServiceSampe0301.java
@@ -46,10 +46,14 @@ package com.example.androidsample.app;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.work.Data;
 import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 
 import com.example.androidsample.R;
@@ -63,7 +67,11 @@ import java.util.concurrent.TimeUnit;
  * 　対象URL：https://akira-watson.com/android/intentservice.html
  *
  * [処理内容]
- *  ServiceSampe0301では1秒のスリープを入れて10回カウントしたら終了させる簡単なサンプルです
+ *  ServiceSampe0301では1秒のスリープを入れて10回カウントしたら終了させる簡単なサンプルです。
+ *  「開始ボタン」、「停止ボタン」は以下3パターンの状態変化でアクティブ・非アクティブにします。
+ *  １．background処理開始で開始ボタンを非アクティブ。停止ボタンをアクティブ
+ *  ２．background処理終了で開始ボタンをアクティブ、停止ボタンを非アクティブ
+ *  ３．停止ボタン押下で開始ボタンをアクティブ、停止ボタンを非アクティブにします。
  *
  **************************************
  * 変更履歴:
@@ -85,23 +93,51 @@ public class ServiceSampe0301 extends AppCompatActivity {
         Button startBtn = findViewById(R.id.service_sample0301_start_button);
         Button stopBtn = findViewById(R.id.service_sample0301_stop_button);
         stopBtn.setEnabled(false); // 開始ボタン押下時のみ有効とする
+        TextView resultView = findViewById(R.id.service_sample0301_result_text);
 
         // 開始ボタン押下でバックグランド処理(1度のみ)を実行開始
         startBtn.setOnClickListener(v -> {
+            WorkManager manager = WorkManager.getInstance(getApplicationContext());
             OneTimeWorkRequest workRequest =
                     new OneTimeWorkRequest.Builder(ServiceSampe0301TestService.class)
-                    .setInitialDelay(5, TimeUnit.SECONDS) // 5秒後に実行開始
+                    .setInitialDelay(5, TimeUnit.SECONDS) // 5秒後に開始
                     .addTag(WORKER_TAG)
                     .build();
-            WorkManager.getInstance(getApplicationContext()).enqueue(workRequest);
+            manager.enqueue(workRequest);
+            // ボタン状態遷移パターン1を設定
+            startBtn.setEnabled(false);
             stopBtn.setEnabled(true);
+
+            // 結果出力エリアをクリア
+            resultView.setText("");
+
+            // Worker完了時かキャンセルされた場合は、開始ボタンを再度アクティブにする
+            LiveData<WorkInfo> live = manager.getWorkInfoByIdLiveData(workRequest.getId());
+            live.observe(this, status -> {
+                if(status.getState() == WorkInfo.State.SUCCEEDED
+                 || status.getState() == WorkInfo.State.CANCELLED) {
+                    // ボタン状態遷移パターン2と3を設定
+                    startBtn.setEnabled(true);
+                    stopBtn.setEnabled(false);
+
+                    // workの処理結果を取得。停止した場合は空のDataが戻る(doWork()で値を設定しても
+                    // WorkManagerがその値を無視する)ことに注意
+                    Data resultData = status.getOutputData();
+                    String outStr =
+                            resultData.getString(ServiceSampe0301TestService.RESULT_DATA_KEY);
+                    if(outStr != null) {
+                        resultView.setText(outStr);
+                    }
+                }
+            });
+
         });
 
         // 停止ボタン押下で実行中のbackground処理を停止
+        // 停止をした場合、Workの処理結果が無視されてしまうことに注意すること
         stopBtn.setOnClickListener(v -> {
             WorkManager.getInstance(getApplicationContext()).cancelAllWorkByTag(WORKER_TAG);
-            stopBtn.setEnabled(false);
+            resultView.setText("キャンセルされました");
         });
-
     }
 }
--- app/src/main/java/com/example/androidsample/app/ServiceSampe0301TestService.java
+++ app/src/main/java/com/example/androidsample/app/ServiceSampe0301TestService.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.work.Data;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
@@ -25,6 +26,18 @@ import androidx.work.WorkerParameters;
  */
 public class ServiceSampe0301TestService extends Worker {
 
+    /** 出力結果を取得するためのキー */
+    public static final String RESULT_DATA_KEY = "result_data";
+
+    // 停止が呼び出された場合
+    private boolean stopFlg = false;
+
+    /**
+     * コンストラクタ：アンドロイドフレームワークにて呼び出されます。
+     *
+     * @param context コンテキスト
+     * @param workerParams　パラメータ
+     */
     public ServiceSampe0301TestService(
             @NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
@@ -34,18 +47,45 @@ public class ServiceSampe0301TestService extends Worker {
     @Override
     public Result doWork() {
         Log.d("debug", "doWork start");
+
+        StringBuilder resultDataStr = new StringBuilder(100);
+        resultDataStr.append("doWork start\n");
+
         try {
-            for(int i = 0; i < 10; i++ ) {
+            for(int i = 0; i < 10 && !stopFlg; i++ ) {
                 Thread.sleep(1000);
+
+                // 何回目のスリープ完了かを出力
+                resultDataStr.append("sleep:");
+                resultDataStr.append(i);
+                resultDataStr.append("\n");
+
                 Log.d("debug", "sleep: " + i);
+
             }
         } catch (InterruptedException ex) {
+            resultDataStr.append("InterruptedException\n");
             Log.d("debug", "InterruptedException");
+
             // このスレッドを止める
             Thread.currentThread().interrupt();
             return Result.failure();
         }
+        resultDataStr.append("doWork end\n");
         Log.d("debug", "doWork end");
-        return Result.success();
+
+        // 出力結果のデータを作成
+        Data resultData = new Data.Builder().putString(RESULT_DATA_KEY, resultDataStr.toString())
+                .build();
+
+        return Result.success(resultData);
     }
+
+    @Override
+    public void onStopped() {
+        // 停止を呼び出された場合
+        Log.d("debug", "onStopped　call");
+        stopFlg = true;
+    }
+
 }
